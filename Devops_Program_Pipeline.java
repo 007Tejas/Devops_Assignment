385a5789ac57e05d04c284b0697cd03fc66927f0
007Tejas <167860760+007Tejas@users.noreply.github.com>
2024-06-07 13:35:13 +0530

    Update Devops_Program_Pipeline.java

--- Devops_Program_Pipeline.java
+++ Devops_Program_Pipeline.java
@@ -1,6 +1,6 @@
 public class DevopsExample {
 
-	public static void public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Welcome to Devops");
 	}
 }
